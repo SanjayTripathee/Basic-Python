fc35026b79d36515387654bf214b747c797c90a6
sanjay <st1706851@gmail.com>
2025-04-08 07:39:10 +0545

    return Function

---
+++ 05_function/02_solution.py
@@ -0,0 +1,7 @@
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+
+def sum(n1,n2):
+    return n1+n2
+
+result = sum(2,2)
+print(result)
---
+++ 05_function/03_solution.py
@@ -0,0 +1,11 @@
+# Problem: Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def multiply(a, b):
+    return a * b
+
+result1 = multiply(2, 3)
+result2 = multiply("h", 3)
+result3 = multiply("a", 3)
+print(result1)
+print(result2)
+print(result3)
