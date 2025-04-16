a0f071e96c1d5b21b000f103e596ee1028dacd39
sanjay <st1706851@gmail.com>
2025-04-16 08:09:33 +0545

    yield concept in function

--- 05_function/08_solution.py
+++ 05_function/08_solution.py
@@ -1,17 +1,17 @@
 # Problem: Create a function that accepts any number of keyword arguments and prints them in the format key: value.
 
-# def print_kwargs(**kwargs):
-#     for key in kwargs:
-#         print(key, ":", kwargs[key])
+# def pair(name,value):
+#     print(name,":",value)
 
-
-
-# print(print_kwargs(a=1, b=2, c=3))
-
-def pair(name,value):
-    print(name,":",value)
-
-pair("a",1)
-pair("b",2)
-pair("c",3)
+# pair("a",1)
+# pair("b",2)
+# pair("c",3)
    
+
+def pair(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key}:{value}")
+
+pair(a=1, b=2, c=3)
+pair(name="sanjay",Power="selfmade")
+pair(f="22")
---
+++ 05_function/09_solution.py
@@ -0,0 +1,10 @@
+# Problem: Write a generator function that yields even numbers up to a specified limit.
+
+def even_num(limit):
+    # li = []
+    for i in range(2,limit+1,2):
+        # li.append(i)
+        var = i
+        return var
+
+print(even_num(10))
